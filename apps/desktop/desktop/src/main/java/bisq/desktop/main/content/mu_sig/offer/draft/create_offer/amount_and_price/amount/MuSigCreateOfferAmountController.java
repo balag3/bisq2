96400ca41f1f91bd337ad2bfbe73e35d515c82ea
HenrikJannsen <boilingfrog@gmx.com>
2026-04-27 09:12:56 +0700

    Refactor: Move into draft folder

---
+++ apps/desktop/desktop/src/main/java/bisq/desktop/main/content/mu_sig/offer/draft/create_offer/amount_and_price/amount/MuSigCreateOfferAmountController.java
@@ -0,0 +1,154 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.desktop.main.content.mu_sig.offer.draft.create_offer.amount_and_price.amount;
+
+import bisq.common.monetary.TradeAmount;
+import bisq.common.observable.Pin;
+import bisq.desktop.common.Browser;
+import bisq.desktop.common.threading.UIThread;
+import bisq.desktop.common.utils.KeyHandlerUtil;
+import bisq.desktop.common.view.Controller;
+import bisq.desktop.main.content.mu_sig.offer.draft.create_offer.amount_and_price.amount.container.MuSigAmountContainerController;
+import bisq.desktop.navigation.NavigationTarget;
+import bisq.i18n.Res;
+import bisq.offer.mu_sig.draft.CreateOfferDraftWorkflow;
+import bisq.offer.price.spec.MarketPriceSpec;
+import bisq.offer.price.spec.PriceSpec;
+import bisq.presentation.formatters.AmountFormatter;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Region;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+@Slf4j
+public class MuSigCreateOfferAmountController implements Controller {
+    private static final PriceSpec MARKET_PRICE_SPEC = new MarketPriceSpec();
+
+    private final MuSigCreateOfferAmountModel model;
+    @Getter
+    private final MuSigCreateOfferAmountView view;
+    private final CreateOfferDraftWorkflow createOfferDraftWorkflow;
+    private final Region owner;
+    private final Consumer<Boolean> navigationButtonsVisibleHandler;
+    private final Consumer<NavigationTarget> closeAndNavigateToHandler;
+    private final Set<Pin> pins = new HashSet<>();
+
+    public MuSigCreateOfferAmountController(CreateOfferDraftWorkflow createOfferDraftWorkflow,
+                                            Region owner,
+                                            Consumer<Boolean> navigationButtonsVisibleHandler,
+                                            Consumer<NavigationTarget> closeAndNavigateToHandler) {
+        this.createOfferDraftWorkflow = createOfferDraftWorkflow;
+        this.owner = owner;
+        this.navigationButtonsVisibleHandler = navigationButtonsVisibleHandler;
+        this.closeAndNavigateToHandler = closeAndNavigateToHandler;
+        model = new MuSigCreateOfferAmountModel();
+
+        MuSigAmountContainerController muSigAmountComponentsController = new MuSigAmountContainerController(createOfferDraftWorkflow);
+        view = new MuSigCreateOfferAmountView(model, this, muSigAmountComponentsController.getView().getRoot());
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // Lifecycle
+    /* --------------------------------------------------------------------- */
+
+    @Override
+    public void onActivate() {
+        pins.add(createOfferDraftWorkflow.useRangeAmountObservable().addObserver(useRangeAmount -> {
+            UIThread.run(() -> {
+                model.getUseRangeAmount().set(useRangeAmount);
+            });
+        }));
+        pins.add(createOfferDraftWorkflow.userSpecificTradeAmountLimitObservable().addObserver(value -> {
+            UIThread.run(this::applyAmountLimitInfo);
+        }));
+        pins.add(createOfferDraftWorkflow.useBaseCurrencyForAmountInputObservable().addObserver(value -> {
+            UIThread.run(this::applyAmountLimitInfo);
+        }));
+    }
+
+    @Override
+    public void onDeactivate() {
+        pins.forEach(Pin::unbind);
+        pins.clear();
+        navigationButtonsVisibleHandler.accept(true);
+        model.getIsOverlayVisible().set(false);
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // Public API
+    /* --------------------------------------------------------------------- */
+
+    public ReadOnlyBooleanProperty getIsOverlayVisible() {
+        return model.getIsOverlayVisible();
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // UI handlers
+    /* --------------------------------------------------------------------- */
+
+    void onSetUseRangeAmount(boolean value) {
+        createOfferDraftWorkflow.setUseRangeAmount(value);
+    }
+
+    void onKeyPressedWhileShowingOverlay(KeyEvent keyEvent) {
+        KeyHandlerUtil.handleEnterKeyEvent(keyEvent, () -> {
+        });
+        KeyHandlerUtil.handleEscapeKeyEvent(keyEvent, this::onCloseOverlay);
+    }
+
+    void onShowOverlay() {
+        if (!model.getIsOverlayVisible().get()) {
+            navigationButtonsVisibleHandler.accept(false);
+            model.getIsOverlayVisible().set(true);
+        }
+    }
+
+    void onCloseOverlay() {
+        if (model.getIsOverlayVisible().get()) {
+            navigationButtonsVisibleHandler.accept(true);
+            model.getIsOverlayVisible().set(false);
+        }
+    }
+
+    void onLearnHowToBuildReputation() {
+        closeAndNavigateToHandler.accept(NavigationTarget.BUILD_REPUTATION);
+    }
+
+    void onOpenWiki(String url) {
+        Browser.open(url);
+    }
+
+    private void applyAmountLimitInfo() {
+        Optional<TradeAmount> userSpecificTradeAmountLimit = createOfferDraftWorkflow.getUserSpecificTradeAmountLimit();
+        model.getShouldShowAmountLimitInfo().set(userSpecificTradeAmountLimit.isPresent());
+        model.getAmountLimitInfo().set(userSpecificTradeAmountLimit
+                .map(tradeAmount -> createOfferDraftWorkflow.toInputAmount(tradeAmount, true))
+                .map(monetary -> AmountFormatter.formatAmountWithCode(monetary, true))
+                .map(formatted -> Res.get("muSig.offer.create.amount.limitInfo.buyer", formatted))
+                .orElse(""));
+    }
+}
---
+++ apps/desktop/desktop/src/main/java/bisq/desktop/main/content/mu_sig/offer/draft/take_offer/amount/container/MuSigAmountContainerController.java
@@ -0,0 +1,104 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.desktop.main.content.mu_sig.offer.draft.take_offer.amount.container;
+
+import bisq.common.market.Market;
+import bisq.common.observable.Pin;
+import bisq.desktop.common.threading.UIThread;
+import bisq.desktop.common.view.Controller;
+import bisq.desktop.main.content.mu_sig.offer.draft.take_offer.amount.container.fix.MuSigFixAmountController;
+import bisq.desktop.main.content.mu_sig.offer.draft.take_offer.amount.container.limits.MuSigAmountLimitsController;
+import bisq.i18n.Res;
+import bisq.offer.mu_sig.draft.TakeOfferDraftWorkflow;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.fxmisc.easybind.EasyBind;
+import org.fxmisc.easybind.Subscription;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+public class MuSigAmountContainerController implements Controller {
+    private final MuSigAmountContainerModel model;
+    @Getter
+    private final MuSigAmountContainerView view;
+    private final MuSigFixAmountController muSigFixAmountController;
+    private final TakeOfferDraftWorkflow takeOfferDraftWorkflow;
+    private final Set<Subscription> subscriptions = new HashSet<>();
+    private final Set<Pin> pins = new HashSet<>();
+
+    public MuSigAmountContainerController(TakeOfferDraftWorkflow takeOfferDraftWorkflow) {
+        this.takeOfferDraftWorkflow = takeOfferDraftWorkflow;
+        model = new MuSigAmountContainerModel();
+
+        muSigFixAmountController = new MuSigFixAmountController(takeOfferDraftWorkflow);
+        MuSigAmountLimitsController amountLimitsController = new MuSigAmountLimitsController(takeOfferDraftWorkflow);
+
+        view = new MuSigAmountContainerView(model, this,
+                muSigFixAmountController.getView().getRoot(),
+                amountLimitsController.getView().getRoot()
+        );
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // Lifecycle
+    /* --------------------------------------------------------------------- */
+
+    @Override
+    public void onActivate() {
+        applyDescription();
+
+
+        pins.add(takeOfferDraftWorkflow.useBaseCurrencyForAmountInputObservable().addObserver(useBaseCurrencyForAmountInput -> {
+            UIThread.run(this::applyDescription);
+        }));
+
+        subscriptions.add(EasyBind.subscribe(muSigFixAmountController.getIsTextInputFocused(),
+                isTextInputFocused -> {
+                    model.getIsTextInputFocused().set(isTextInputFocused);
+                }));
+    }
+
+    @Override
+    public void onDeactivate() {
+        subscriptions.forEach(Subscription::unsubscribe);
+        subscriptions.clear();
+        pins.forEach(Pin::unbind);
+        pins.clear();
+
+        model.getIsTextInputFocused().unbind();
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // Private
+    /* --------------------------------------------------------------------- */
+
+    private void applyDescription() {
+        Market market = takeOfferDraftWorkflow.getMarket();
+        String code = getCode(market);
+        model.getDescription().set(Res.get("muSig.offer.create.amount.description.fixed", code));
+    }
+
+    private String getCode(Market market) {
+        boolean useBaseCurrencyForAmountInput = takeOfferDraftWorkflow.getUseBaseCurrencyForAmountInput();
+        return useBaseCurrencyForAmountInput ? market.getBaseCurrencyCode() : market.getQuoteCurrencyCode();
+    }
+}
